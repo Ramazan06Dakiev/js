1e402bfb08ea680a664401679e55ea8d6bf49655
Ramazan <ramzan_dakiev@mail.ru>
2020-11-07 23:25:53 +0300

    Добавлены переменные, добавен обработчик событий onclick и  вывод с помощью textContent

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-document.querySelector('.page');
-console.log(document.querySelector('.page'));
-document.querySelector('.page').classList.remove('second-page'); 
+let page = document.querySelector('.page');
+console.log(page);
+page.classList.remove('second-page'); 
+
+let lightButton = document.querySelector('.light-button');
+console.log(lightButton);
+lightButton.onclick = function () {
+    document.querySelector('.light-button');
+    document.querySelector('.light-button').classList.add('light');
+    console.log('кнопка нажата!'); 
+};
+
+let message = document.querySelector('.text');
+console.log(message.textContent);
